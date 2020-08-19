acf8c7c5b31c2b28dee77ed751dd1eb161c8a770
Tanvi <guravtan@gmail.com>
2020-08-19 00:59:11 -0400

    Update

--- Word-Guessing-Game/words.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-const wordList = [ 'START', 'TREES', 'TREAT', 'STARS', 'TRAPS', 'PARTS',
-    'STRAP', 'STARK', 'CARTS', 'SPREE', 'TARPS', 'TARTS',
-    'CRASH', 'TRASH', 'SHARP', 'HARPS', 'CRUSH', 'RUSTS',
-    'RESTS', 'STEER', 'STARE', 'TRACK', 'THESE', 'TRUCK' ]
-    
-module.exports = wordList;
